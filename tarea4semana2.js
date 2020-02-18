49b6118f865abb5f3f956c65d6453b772a9c7008
Matias Amorim <mamorim@altimetrik.com>
2020-02-18 14:32:02 -0300

    Eliminar archivos

--- tarea4semana2.js
+++
@@ -1,7 +0,0 @@
-//////////////////////// Write a JavaScript function to get the day name from a particular date/////////////////////
-var day_name = function(dt){
-    mlist = [ "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
-      return mlist[dt.getDay()];
-    };
-    console.log(day_name(new Date("10/11/2009")));
-    console.log(day_name(new Date("11/13/2014")));
